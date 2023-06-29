38b8d55f444b41a59703c4cb197c49ff0f37e16a
Dave <interstellaryachtclub@gmail.com>
2023-06-29 01:30:10 -0400

    firestore

--- src/auth/firebaseauth.js
+++ src/auth/firebaseauth.js
@@ -1,10 +1,12 @@
 import firebaseconfig from '../config/firebaseconfig';
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const app = initializeApp(firebaseconfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider(app);
+export const db = getFirestore(app);
 
 //Detect auth state
 onAuthStateChanged(auth, (user) => {
--- src/components/Authform.jsx
+++ src/components/Authform.jsx
@@ -3,8 +3,11 @@ import {
   signInWithPopup,
   signOut,
 } from 'firebase/auth';
-import { useState } from 'react';
-import { auth, googleProvider } from '../auth/firebaseauth';
+import { useState, useEffect } from 'react';
+import { auth, googleProvider, db } from '../auth/firebaseauth';
+import { getDocs, collection } from 'firebase/firestore';
+
+
 
 export const Authform = () => {
   const [email, setEmail] = useState('');
@@ -39,22 +42,65 @@ export const Authform = () => {
   //console.log(auth?.currentUser.email);
   //console.log(auth?.currentUser.photoURL);
 
+  const [playlists, setPlaylists] = useState([]);
+  const playlistsCollectionRef = collection(db, "Playlists/marcfifelibrary/crate");
+
+  useEffect(() => {
+    const getPlaylists = async () => {
+      //data
+      //setstate
+      try {
+      const data = await getDocs(playlistsCollectionRef);
+      const filtereddata = data.docs.map((doc) => ({
+        ...doc.data(), id:doc.id,
+      }));
+
+      console.log(filtereddata);
+      setPlaylists(filtereddata);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getPlaylists();
+  }, []);
+
   return (
     <div>
+      <div>
+        //email signin
+      </div>
       <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
       <input
         placeholder="password"
         onChange={(e) => setPasswd(e.target.value)}
       />
+      <button onClick={signIn}>sign in</button>
       <div>
-        <button onClick={signIn}>sign in</button>
-      </div>
-      <div>
+        <div>//google signin</div>
         <button onClick={signIntoGoogle}>sign into google</button>
       </div>
       <div>
         <div>//logout</div>
         <button onClick={signOut}>sign out</button>
+      </div>
+
+      <div>
+        {playlists.map((playlist) => (
+          <div key="playlistInfo">
+            <div>
+            {playlist.name}
+            </div>
+            <div>
+              <p>user: {playlist.ownerName}</p>
+              </div>
+              <ul>
+              {playlist.tracks.map((track) => (
+                console.log(playlist.tracks.docs)
+                  
+              ))}
+              </ul>
+              </div>
+        ))}
       </div>
     </div>
   );
