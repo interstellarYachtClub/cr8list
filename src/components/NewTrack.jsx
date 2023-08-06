be311ca464dd016ce3d99eb8c6a8b35d5ef5a3e0
Dave <interstellaryachtclub@gmail.com>
2023-08-06 17:27:13 -0400

    manual add track key

--- src/components/NewTrack.jsx
+++ src/components/NewTrack.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { manualTrackForm } from '../utilities/formInputs';
+import { trackKeys } from '../utilities/formInputs';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../auth/firebaseauth';
 import { AuthContext } from '../context/AuthContext';
@@ -12,6 +13,13 @@ const NewTrack = () => {
     const value = e.target.value;
     setTrackData({ ...trackData, [id]: value });
   };
+
+  const handleKeySelect = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+    setTrackData({ ...trackData, [id]: value });
+  };
+
   console.log(trackData);
 
   const handleAddTrack = async () => {
@@ -28,7 +36,7 @@ const NewTrack = () => {
         release: trackData.release,
         bpm: trackData.bpm,
         time: trackData.time,
-        key: trackData.key,
+        key: trackData.key_select,
       }
     );
   };
@@ -38,7 +46,7 @@ const NewTrack = () => {
       {manualTrackForm.map((input) => {
         if (input.showfield) {
           return (
-            <div className={input.id} key={input.id}>
+            <div className={input.id + ' flex justify-between'} key={input.id}>
               <label>{input.label}</label>
               <input
                 className="rounded m-2 text-center focus:animate-pulse border-b-2 border-white/[0] focus:outline-none focus:border-b-2 focus:border-blue-950"
@@ -51,7 +59,30 @@ const NewTrack = () => {
           );
         }
       })}
-      <button onClick={handleAddTrack}>Add Track</button>
+      <div className="flex justify-between accent-blue-950">
+        <label for="key_select">Key</label>
+        <select
+          className="accent-blue-950 rounded m-2 text-center focus:animate-pulse border-b-2 border-white/[0] focus:outline-none focus:border-b-2 focus:border-blue-950"
+          name="key_select"
+          id="key_select"
+          onChange={handleKeySelect}
+        >
+          <option value="">select the key for this track</option>
+          {trackKeys.map((key) => {
+            return (
+              <option
+                id={'keyid' + key.id}
+                value={key.short + ' / ' + key.openkey}
+              >
+                {key.camelot} / {key.long} / {key.openkey}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div>
+        <button onClick={handleAddTrack}>Add Track</button>
+      </div>
     </div>
   );
 };
